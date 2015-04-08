85f4861c25b8451c7c59ac1af7a44c507d744c45
Aurelien Giraud <aure@posteo.de>
2015-04-08 15:47:54 +0000

    Inserting stories with a date and without any date field in the form is working. Still have to find out how to display the date with a better format.

--- client/autoformHooks/stories.js
+++ client/autoformHooks/stories.js
@@ -1,13 +1,14 @@
 var storyHooks = {
   before: {
     insert: function(doc) {
-      doc.created = new date();
-    return doc;
+      doc.submitted = new Date();
+      return doc;
     }
   }
 }
 
-//AutoForm.addHooks('insertStoryForm', storyHooks);
+/*AutoForm.addHooks('insertStoryForm', storyHooks);*/
+
 AutoForm.hooks({
  insertStoryForm: storyHooks
 });

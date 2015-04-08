685d605d8b8ea513b51806e3729b9261fe2f14e6
Aurélien Giraud <aure@posteo.de>
2015-04-08 14:45:14 +0200

    inserting a story does not work

--- client/autoformHooks/stories.js
+++ client/autoformHooks/stories.js
@@ -1,8 +1,13 @@
-var storyHooks = {
-  formToDoc: function(doc) {
-      doc.createdDate = moment();
-    return doc;
-  }
-}
+// var storyHooks = {
+//   before: {
+//     insert: function(doc) {
+//       doc.created = new date();
+//     return doc;
+//     }
+//   }
+// }
 
-AutoForm.addHooks('insertStoryForm', storyHooks);
+// AutoForm.addHooks('insertStoryForm', storyHooks);
+// AutoForm.hooks({
+//   insertStoryForm: storyHooks
+// });
